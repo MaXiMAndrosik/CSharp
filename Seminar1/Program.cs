506e68d93c2168044ae0f5b283017b2454d1787f
“MAXIM <androsikmaksim@gmail.com>
2023-12-23 21:26:50 +0300

    add Seminar1:Task1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,19 @@
+﻿// ЗАДАЧА 1
+
+// Console.Clear();
+// int max = 0; int min = 0;
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1>num2)
+//      {max = num1; min = num2;
+//      Console.WriteLine("Первое число '"+num1+"' больше чем второе число '"+num2+"'");}
+// if (num1<num2)  
+//      {max = num2; min = num1;
+//     Console.WriteLine("Первое число '"+num1+"' меньше чем второе число '"+num2+"'");}
+// if (num1==num2)
+//      Console.WriteLine("Введенные числа равны '"+num1+"'");
+
+// ЗАДАЧА 2
