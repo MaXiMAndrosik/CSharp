e70c2cc914839c855ffe0f0df3047f59c1c40709
“MAXIM <androsikmaksim@gmail.com>
2023-12-23 21:48:38 +0300

    add Seminar1:Task2

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -4,7 +4,7 @@
 // int max = 0; int min = 0;
 // Console.Write("Введите первое число: ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите первое число: ");
+// Console.Write("Введите второе число: ");
 // int num2 = Convert.ToInt32(Console.ReadLine());
 
 // if (num1>num2)
@@ -17,3 +17,17 @@
 //      Console.WriteLine("Введенные числа равны '"+num1+"'");
 
 // ЗАДАЧА 2
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2>max)
+     max = num2;
+if (num3>max)
+     max = num3;
+Console.Write("Максимальное число: '"+max+"'");
