16d2219c5e703d714f97b2cd00d830c1a0ee0d44
“MAXIM <androsikmaksim@gmail.com>
2023-12-23 22:50:10 +0300

    add Seminar1:Task3

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -18,16 +18,26 @@
 
 // ЗАДАЧА 2
 
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+// if (num2>max)
+//      max = num2;
+// if (num3>max)
+//      max = num3;
+// Console.Write("Максимальное число: '"+max+"'");
+
+// ЗАДАЧА 3
+
 Console.Clear();
-Console.Write("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третье число: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
-int max = num1;
-if (num2>max)
-     max = num2;
-if (num3>max)
-     max = num3;
-Console.Write("Максимальное число: '"+max+"'");
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2 == 0)
+    Console.Write("Число '"+number+"' четное");
+else
+    Console.Write("Число '"+number+"' нечетное");
